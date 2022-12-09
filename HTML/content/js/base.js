888056af4e611b9015cc02f971ced87c59d3f75a
efsanbbasi <107312655+blasedazaii@users.noreply.github.com>
2022-12-09 16:19:35 +0300

    ödev

---
+++ HTML/content/js/base.js
@@ -0,0 +1,24 @@
+
+function test() {
+    alert("test");
+}
+
+function setGreen() {
+    document.querySelector("#textFormatter").classList.remove("red");
+    document.querySelector("#textFormatter").classList.add("green");
+}
+
+function setRed() {
+    document.querySelector("#textFormatter").classList.remove("green");
+    document.querySelector("#textFormatter").classList.add("red");
+}
+
+function submitform() {
+    //validation kontrol edilecek
+    var isValid = true;
+    if (isValid) {
+        debugger
+        document.getElementById("form").submit();
+
+    }
+}
